c019b085c096871ebc1ba7f6f7d8a83ba450bd0b
Johnbyju <johnbyju8@gmail.com>
2024-11-12 15:46:15 +0530

    Update Bgvedio.jsx

--- src/component/Bg-vedio/Bgvedio.jsx
+++ src/component/Bg-vedio/Bgvedio.jsx
@@ -6,7 +6,7 @@ const Bgvedio = () => {
     <>
       <div className="video-container min-h-screen relative" >
         <div className='linear-bg min-h-screen'>
-          <video autoPlay loop muted className="background-video">
+          <video autoPlay loop muted playsInline className="background-video">
             <source src="https://sapphire-large-cougar-300.mypinata.cloud/ipfs/QmWaqmrEnEHUhu4gDHhdV87rk8w4qFqjQUoFCuXicE8seD" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
