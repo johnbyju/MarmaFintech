d29c46f81a3710b71e917051b9d415897bd583c0
Johnbyju <johnbyju8@gmail.com>
2024-12-16 17:39:10 +0530

    the changes in the css file for the bg-blur

--- src/component/Border/Border.jsx
+++ src/component/Border/Border.jsx
@@ -4,7 +4,6 @@ import Explore from '../Explore/Explore'
 import Bgvedio from '../Bg-vedio/Bgvedio'
 import Contact from '../contact/Contact'
 import Openings from '../Openings/Openings'
-import Subscription from '../subscription/Subscription'
 import ExtendMission from '../Mission/ExtendMission'
 import Menu from '../Menuicon/Menu'
 import Extendproduct from '../Product/Extendproduct'
@@ -13,51 +12,17 @@ const Border = () => {
     return (
         <>
             <div>
-                <Menu />
+                <Menu/>
                 <Home/>
-                <Bgvedio />
-                <ExtendMission />
-            <Extendproduct />
+                <Bgvedio/>
+                <ExtendMission/>
+                <Extendproduct/>
                 <Team />
                 <Openings />
                 <Explore />
                 <Contact />
             </div>
         </>
-
-        //     <div className='root-wrapper p-8 flex flex-col'>
-        //         <div className="
-        //   border-class
-        //     border border-customgrey
-        //     rounded-lg 
-        //     relative 
-        //     flex 
-        //     flex-col 
-        //     items-center 
-
-        //     overflow-y-auto      
-        //     z-10
-        //   "   style={{
-        //                 borderWidth: '1px',
-        //                 borderColor: '#424242',
-        //                 padding: '0px'
-        //             }} >
-        //             <Menu />
-        //             <Home />
-        //             <Bgvedio />
-        //             <ExtendMission />
-        //             {/* <Product/> */}
-        //             <Extendproduct/>
-        //             <Team />
-        //             <Openings />
-        //             <Explore />
-        //             <Subscription />
-        //             <Contact />
-        //         </div>
-
-        //     </div>
-
     )
 }
-
 export default Border
