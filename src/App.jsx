76d2113b24964b32fafe36c8e8bd009f586f96bc
Johnbyju <johnbyju8@gmail.com>
2024-12-17 20:38:31 +0530

    menu icon content

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import { Canvas } from '@react-three/fiber';
+import React, { useEffect } from 'react';
 import './App.css';
-import React, { useEffect, useRef } from 'react';
-import { Environment, OrbitControls } from '@react-three/drei';
 import Border from './component/Border/Border';
+import disableDevtool from 'disable-devtool';
 
 
-
+// Added disable devtool for user can't able to open the devtool
 const App = () => {
-  return (
-    <>
+  // useEffect(() => {
+    
+  //   disableDevtool({
+  //     debug: false, 
+  //     ondevtoolopen: () => {}, 
+  //   });
+  // }, []);
 
-          <Border/>
-    
-      {/* <Canvas>
-      </Canvas> */}
-        {/* <ambientLight intensity={0} />
-        <directionalLight position={[0, 0, 0]} intensity={0} />
-        <OrbitControls enableRotate={true} autoRotate={0}   enableZoom={false}/>
-        <Earth/>
-        <Environment preset="sunset" /> */}
-    
-     
-    </>
+  return (
+      <>
+        <Border />
+      </>
   );
 };
 
--- src/component/Menuicon/Menu.jsx
+++ src/component/Menuicon/Menu.jsx
@@ -9,9 +9,9 @@ export default function Menu() {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () =>{
-    setIsOpen(!isOpen); 
-  } 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,7 +29,7 @@ export default function Menu() {
 
   return (
     <div className="hamburger-menu ">
-      <button     
+      <button
         onClick={toggleMenu}
         className={`hamburger-button ${isOpen ? 'open' : ''}`}
         aria-label="Toggle menu"
@@ -39,27 +39,38 @@ export default function Menu() {
       </button>
 
       {isOpen && (
-        
-          <nav className="menu-content  bg-[#FFFFFF1A]   rounded-lg  backdrop-blur-md">
-            <ul>
-              <li><a href="#mission" onClick={toggleMenu}>Mission</a></li>
-              <li><a href="#products" onClick={toggleMenu}>Products</a></li>
-              <li><a href="#team" onClick={toggleMenu}>Team</a></li>
-              <li><a href="#event" onClick={toggleMenu}>Event</a></li>
-              <li><a href="#contact" onClick={toggleMenu} className="contact-us">Contact us</a></li>
-            </ul>
-            <div className="social-icons">
-              <a href="#" aria-label="Twitter" className='custom-icon'>
+
+        <nav className="menu-content  bg-[#FFFFFF1A]   rounded-lg  backdrop-blur-md">
+          <ul>
+            <li>
+              <a href="#mission" onClick={toggleMenu}>Mission</a>
+            </li>
+            <li>
+              <a href="#products" onClick={toggleMenu}>Products</a>
+            </li>
+            <li>
+              <a href="#team" onClick={toggleMenu}>Team</a>
+            </li>
+            <li>
+              <a href="#event" onClick={toggleMenu}>Event</a>
+            </li>
+            <li>
+              <a href="#contact" onClick={toggleMenu} className="contact-us">Contact us</a>
+            </li>
+          </ul>
+
+          <div className="social-icons">
+            <a href="#" aria-label="Twitter" className='custom-icon'>
               <img src='/icons/x.png' alt="" />
-              </a>
-              <a href="#" aria-label="LinkedIn" className='custom-icon'>
+            </a>
+            <a href="#" aria-label="LinkedIn" className='custom-icon'>
               <img src='/icons/linkedin.png' alt="" />
-              </a>
-              <a href="#" aria-label="Instagram" className='custom-icon'>
+            </a>
+            <a href="#" aria-label="Instagram" className='custom-icon'>
               <img src='/icons/insta.png' alt="" />
-              </a>
-            </div>
-          </nav>
+            </a>
+          </div>
+        </nav>
       )}
     </div>
   );
